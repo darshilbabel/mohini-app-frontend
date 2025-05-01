d7f035db6966a9d6254ba862a9c767574de57eb9
KUNALTEMPEST <kunalpratapsinghofficial@gmail.com>
2025-05-01 07:29:54 +0530

    Blank Page fix

--- src/pages/shikshalokam.js
+++ src/pages/shikshalokam.js
@@ -1,9 +1,16 @@
+import { useEffect } from "react";
 import Login from "../components/Login";
 
 
 
 function Shikshalokam({type, variant}) {
 
+	useEffect(() => {
+		document.cookie.split(";").forEach((cookie) => {
+			const name = cookie.split("=")[0].trim();
+			document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+		});
+	}, []);
 
 	return (
 		<Login type={type || "shikshalokam"} variant={variant || ""}/>
