b5d64df151407193c2f3d7c6b5228c56506581c5
KUNALTEMPEST <kunalpratapsinghofficial@gmail.com>
2025-06-04 10:31:53 +0530

    Fix

--- src/components/Popup/languageSelection.jsx
+++ src/components/Popup/languageSelection.jsx
@@ -22,7 +22,7 @@ const LanguageSelectionPopup = ({ languageList, selectedLanguage, onSelect, onCl
                 <div className="language-select-container">
                     <div className="language-select-content px-0">
                         <p className="mb-4 sm:text-xl text-lg font-bold text-center">{t('languageQuestion')}</p>
-                        <div className="flex flex-wrap gap-x-2 gap-y-2 md:gap-y-10 justify-center">
+                        <div className="flex flex-wrap gap-x-2 gap-y-2 md:gap-y-4 justify-center">
                         {languageList.map((lang) => (
                             <div
                                 key={lang.value}
