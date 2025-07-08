855e7ca3403a4ba9d9fa331ef07ddc3d73099717
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2025-07-08 17:11:56 +0530

    feat: Update audio paths to use environment variable for dynamic loading

--- src/services/const/questions.js
+++ src/services/const/questions.js
@@ -8,12 +8,12 @@ const questions = {
         title: {
           en: {
             text: "What is your name and what are you doing at this meeting?",
-            audio: '/mohini/audio/q1v1_en.b64',
+            audio: `${process.env.REACT_APP_ADUIO_PATH ?? ""}/audio/q1v1_en.b64`,
             
           },
           te: {
             text: "మీ పేరు మరియు మీరు ఈ సమావేశంలో మీరు ఏమి చేస్తున్నారు?",
-            audio: '/mohini/audio/q1v1_te.b64',
+            audio: `${process.env.REACT_APP_ADUIO_PATH ?? ""}/audio/q1v1_te.b64`,
             
           },
           
@@ -33,12 +33,12 @@ const questions = {
         title: {
           en: {
             text: "Please tell me the name of the district you are in.",
-            audio: '/mohini/audio/q2v1_en.b64',
+            audio: `${process.env.REACT_APP_ADUIO_PATH ?? ""}/audio/q2v1_en.b64`,
             
           },
           te: {
             text: "మీరు ఉన్న జిల్లా పేరు చెప్పండి.",
-            audio: '/mohini/audio/q2v1_te.b64',
+            audio: `${process.env.REACT_APP_ADUIO_PATH ?? ""}/audio/q2v1_te.b64`,
             
           },
           
@@ -58,12 +58,12 @@ const questions = {
         title: {
           en: {
             text: "Please tell me the name of the school you are in.",
-            audio: '/mohini/audio/q3v1_en.b64',
+            audio: `${process.env.REACT_APP_ADUIO_PATH ?? ""}/audio/q3v1_en.b64`,
             
           },
           te: {
             text: "మీరు ఉన్న పాఠశాల పేరు చెప్పండి.",
-            audio: '/mohini/audio/q3v1_te.b64',
+            audio: `${process.env.REACT_APP_ADUIO_PATH ?? ""}/audio/q3v1_te.b64`,
             
           },
           
@@ -83,12 +83,12 @@ const questions = {
         title: {
           en: {
             text: "Was there anything that stood out to you in today's meeting?",
-            audio: '/mohini/audio/q4v1_en.b64',
+            audio: `${process.env.REACT_APP_ADUIO_PATH ?? ""}/audio/q4v1_en.b64`,
             
           },
           te: {
             text: "ఈ రోజు జరిగిన సమావేశంలో మీకు ప్రత్యేకంగా అనిపించిన విషయం ఏమైనా ఉందా?",
-            audio: '/mohini/audio/q4v1_te.b64',
+            audio: `${process.env.REACT_APP_ADUIO_PATH ?? ""}/audio/q4v1_te.b64`,
             
           },
           
@@ -108,12 +108,12 @@ const questions = {
         title: {
           en: {
             text: "Describe in detail how you felt about the school atmosphere today.",
-            audio: '/mohini/audio/q5v1_en.b64',
+            audio: `${process.env.REACT_APP_ADUIO_PATH ?? ""}/audio/q5v1_en.b64`,
             
           },
           te: {
             text: "ఇవాళ పాఠశాల వాతావరణం మీకు ఎలా అనిపించిందో వివరంగా చెప్పండి.",
-            audio: '/mohini/audio/q5v1_te.b64',
+            audio: `${process.env.REACT_APP_ADUIO_PATH ?? ""}/audio/q5v1_te.b64`,
             
           },
           
@@ -133,12 +133,12 @@ const questions = {
         title: {
           en: {
             text: "How did you feel about the quality of the discussions held at this meeting?",
-            audio: '/mohini/audio/q6v1_en.b64',
+            audio: `${process.env.REACT_APP_ADUIO_PATH ?? ""}/audio/q6v1_en.b64`,
             
           },
           te: {
             text: "ఈ సమావేశంలో జరిగిన చర్చల నాణ్యత గురించి మీకు ఎలా అనిపించింది?",
-            audio: '/mohini/audio/q6v1_te.b64',
+            audio: `${process.env.REACT_APP_ADUIO_PATH ?? ""}/audio/q6v1_te.b64`,
             
           },
           
@@ -158,12 +158,12 @@ const questions = {
         title: {
           en: {
             text: "What changes do you hope will happen in the school after this meeting?",
-            audio: '/mohini/audio/q7v1_en.b64',
+            audio: `${process.env.REACT_APP_ADUIO_PATH ?? ""}/audio/q7v1_en.b64`,
             
           },
           te: {
             text: "ఈ సమావేశం తర్వాత పాఠశాలలో మీరు ఏ మార్పులు జరుగుతాయి అని ఆశిస్తున్నారు?",
-            audio: '/mohini/audio/q7v1_te.b64',
+            audio: `${process.env.REACT_APP_ADUIO_PATH ?? ""}/audio/q7v1_te.b64`,
             
           },
           
