cd38e2b826b7d9d548ac76a757b1a3573488c021
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2025-09-11 17:33:43 +0530

    pass index

--- src/pages/shikshagraha-repository/listing/BrowseResources.jsx
+++ src/pages/shikshagraha-repository/listing/BrowseResources.jsx
@@ -253,7 +253,7 @@ export default function BrowseResources({
         {resources.map((resource, index) => (
           <React.Fragment key={`resource-${resource.id}-${index}`}>
             {" "}
-            <ResourceCard key={resource.id} resource={resource} />
+            <ResourceCard key={resource.id} resource={resource} index={index} />
           </React.Fragment>
         ))}
       </div>
