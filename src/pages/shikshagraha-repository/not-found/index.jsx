c6cd8aec9dc9f02f6b56a507809e997fb541e906
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2025-09-11 18:35:50 +0530

    not found page

---
+++ src/pages/shikshagraha-repository/not-found/index.jsx
@@ -0,0 +1,20 @@
+import { GrEmptyCircle } from "react-icons/gr";
+import Footer from "../common/Footer";
+import Header from "../listing/Header";
+
+function NotFound() {
+  return (
+    <div>
+      <Header isHeroSection={false} />
+      <div className="flex flex-col items-center justify-center h-screen">
+        <GrEmptyCircle size={100} />
+        <h1 className="text-2xl font-bold text-center mt-20">404 Not Found</h1>
+        <p className="text-center">
+          The page you are looking for does not exist.
+        </p>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+export default NotFound;
