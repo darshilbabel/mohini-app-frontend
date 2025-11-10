41f200d4c9081b3fc5023963186e09a89e6a7dad
Mohit Bharti <mohitbharti373@gmail.com>
2025-11-10 14:34:09 +0530

    commented out wss

--- src/pages/ShikshalokamVoiceChat/voice-chat.js
+++ src/pages/ShikshalokamVoiceChat/voice-chat.js
@@ -63,8 +63,8 @@ import WaveSurferPlayer from "../interview-text-voice/voice-player"
 const cookies = new Cookies()
 
 // TODO: After testing, revert this to the original code
-// const wss_protocol = window.location.protocol === "https:" ? "wss://" : "ws://"
-const wss_protocol = "wss://"
+const wss_protocol = window.location.protocol === "https:" ? "wss://" : "ws://"
+// const wss_protocol = "wss://"
 
 const ShikshalokamVoiceBasedChat = ({ type = "", variant = "" }) => {
   // ========== useState Hooks ==========
