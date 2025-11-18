a07af9bd9c6448e95eeb8f0ea3f81403da46cc48
Vishnu Krishnathu <54522897+VishnuKrishnathu@users.noreply.github.com>
2025-11-18 18:05:40 +0530

    Refactor ApiClient methods for consistency and readability
    - Removed unnecessary semicolons and standardized formatting in index.js.
    - Ensured consistent use of imports and method calls throughout the ApiClient class.

--- src/api/client/index.js
+++ src/api/client/index.js
@@ -1,36 +1,36 @@
-import axios from "axios";
-import env from "@/utils/env";
+import axios from "axios"
+import env from "utils/env"
 
 class ApiClient {
   constructor(httpClient) {
-    this.client = httpClient;
+    this.client = httpClient
   }
 
   async get(url, config) {
-    return this.client.get(url, config);
+    return this.client.get(url, config)
   }
 
   async post(url, data, config) {
-    return this.client.post(url, data, config);
+    return this.client.post(url, data, config)
   }
 
   async put(url, data, config) {
-    return this.client.put(url, data, config);
+    return this.client.put(url, data, config)
   }
 
   async delete(url, config) {
-    return this.client.delete(url, config);
+    return this.client.delete(url, config)
   }
 
   async patch(url, data, config) {
-    return this.client.patch(url, data, config);
+    return this.client.patch(url, data, config)
   }
 }
 
 const axiosInstance = axios.create({
   withCredentials: false,
   baseURL: env.LOCAL_PROXY(),
-  params: {}, 
-});
+  params: {},
+})
 
-export const apiClient = new ApiClient(axiosInstance);
+export const apiClient = new ApiClient(axiosInstance)
