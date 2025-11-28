b4f939f1ea861967fe0de26ad93c6e8b6cdd47a1
Pankaj Ganjale <pankajganjale2805@gmail.com>
2025-11-28 08:31:11 +0530

    Removed click from aside, move aside text to translation, Added 3 line visibility and 250 max char limit in input

--- src/pages/shikshagraha-repository/listing/MitraAiAssistantAside.jsx
+++ src/pages/shikshagraha-repository/listing/MitraAiAssistantAside.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { MdMoreVert } from "react-icons/md";
 import ROUTES from "../../../url";
 
 export default function MitraAiAssistantAside() {
+  const { t } = useTranslation();
   const handleClick = () => {
     // Open Mitra AI assistant page in a new tab
     const fullUrl = `${window.location.origin}${ROUTES.SHIKSHAGRAHA_REPOSITORY_MITRA_AI_ASSISTANT}`;
@@ -10,20 +12,18 @@ export default function MitraAiAssistantAside() {
   };
 
   return (
-    <aside className="bg-white px-4 py-6 h-full" onClick={handleClick}>
+    <aside className="bg-white px-4 py-6 h-full">
       <div className="flex items-center justify-between">
-        <h3 className="text-lg font-bold leading-[28px]">Mitra AI Assistant</h3>
+        <h3 className="text-lg font-bold leading-[28px]">{t("mitraAiAssistant")}</h3>
         <MdMoreVert className="w-5 h-5 text-[#9CA3AF]" />
       </div>
       <div className="h-[1px] border-t border-[#E5E7EB] my-4"></div>
-      <div className="rounded-lg p-3 bg-[#F3F4F6] cursor-pointer">
+      <div className="rounded-lg p-3 bg-[#F3F4F6] cursor-pointer" onClick={handleClick}>
         <h4 className="font-medium text-sm leading-[20px]">
-          Generate Micro Improvement Projects (MIPs)
+          {t("generateMicroImprovementProjects")}
         </h4>
         <p className="font-normal text-xs leading-[16px] text-[#6B7280] mt-[10px]">
-          MItra AI asks questions to understand your challenges, objectives,
-          tasks, along with observations and surveys to generate tailored
-          solutions...
+          {t("generateMicroImprovementProjectsDescription")}
         </p>
       </div>
     </aside>
