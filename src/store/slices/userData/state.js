852e07811044da7d28cc52189eccb8999e6b7365
Vishnu Krishnathu <54522897+VishnuKrishnathu@users.noreply.github.com>
2025-12-04 18:50:29 +0530

    Add IP Zip Code management in ShikshalokamChat and ShikshalokamVoiceChat components

--- src/store/slices/userData/state.js
+++ src/store/slices/userData/state.js
@@ -7,11 +7,14 @@ export const INITIAL_STATE = (set, get, store) => ({
   has_accepted_tnc: "ONGOING",
   ipCity: null,
   ipCountry: null,
+  ipZipCode: null,
   ipState: null,
   preferredLanguage: null,
   profileId: null,
   state: null,
   userId: null,
+
+  setIpZipCode: ipZipCode => set({ ipZipCode }),
 
   setPrefferedLanguage: preferredLanguage => set({ preferredLanguage }),
 
